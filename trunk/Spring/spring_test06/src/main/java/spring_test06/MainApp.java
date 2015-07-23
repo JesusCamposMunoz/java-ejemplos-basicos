44566485e7abe11a91b6501b6d24742eca1b7839
campos.tenia.que.ser@gmail.com <campos.tenia.que.ser@gmail.com@04c2318c-9c4d-3ac3-30d0-d60898d78f61>
2015-07-23 09:00:11 +0000

    @PostConstruct y @PreDestroy.

---
+++ trunk/Spring/spring_test06/src/main/java/spring_test06/MainApp.java
@@ -0,0 +1,18 @@
+package spring_test06;
+/**
+ * Uso de la anotaciones @PostConstruct and @PreDestroy.
+ */
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+   public static void main(String[] args) {
+
+      AbstractApplicationContext context = 
+                          new ClassPathXmlApplicationContext("Beans.xml");
+
+      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+      obj.getMessage();
+      context.registerShutdownHook();
+   }
+}
